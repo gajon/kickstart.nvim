6e1d25e862c459216450e28de98d07c6589a3622
Jorge Gajon <gajon@gajon.org>
2026-05-24 23:32:03 -0600

    custom/plugins/colorschemes.lua and added NeoSolarized

---
+++ lua/custom/plugins/colorschemes.lua
@@ -0,0 +1,56 @@
+-- You can easily change to a different colorscheme.
+-- Change the name of the colorscheme plugin below, and then
+-- change the command under that to load whatever the name of that colorscheme is.
+--
+-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+
+do -- NeoSolarized
+  vim.pack.add { 'https://github.com/overcache/NeoSolarized' }
+
+  -- Default value is "normal", Setting this option to "high" or "low" does use the
+  -- same Solarized palette but simply shifts some values up or down in order to
+  -- expand or compress the tonal range displayed.
+  vim.g.neosolarized_contrast = "normal"
+
+  -- Special characters such as trailing whitespace, tabs, newlines, when displayed
+  -- using ":set list" can be set to one of three levels depending on your needs.
+  -- Default value is "normal". Provide "high" and "low" options.
+  vim.g.neosolarized_visibility = "normal"
+
+  -- I make vertSplitBar a transparent background color. If you like the origin
+  -- solarized vertSplitBar style more, set this value to 0.
+  vim.g.neosolarized_vertSplitBgTrans = 1
+
+  -- If you wish to enable/disable NeoSolarized from displaying bold, underlined
+  -- or italicized" typefaces, simply assign 1 or 0 to the appropriate variable.
+  -- Default values:
+  vim.g.neosolarized_bold = 1
+  vim.g.neosolarized_underline = 1
+  vim.g.neosolarized_italic = 1
+
+  -- Used to enable/disable "bold as bright" in Neovim terminal. If colors of bold
+  -- text output by commands like `ls` aren't what you expect, you might want to
+  -- try disabling this option. Default value:
+  vim.g.neosolarized_termBoldAsBright = 1
+end
+
+do -- tokyonight
+  vim.pack.add { 'https://github.com/folke/tokyonight.nvim' }
+  ---@diagnostic disable-next-line: missing-fields
+  require('tokyonight').setup {
+    styles = {
+      comments = { italic = true }, -- Enable italics in comments
+    },
+  }
+  -- Like many other themes, this one has different styles, and you could load
+  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  -- 'tokyonight-moon'
+end
+
+-- NeoSolarized
+-- tokyonight-day
+-- tokyonight-moon
+-- tokyonight-night
+-- tokyonight-storm
+vim.opt.background = 'dark'
+vim.cmd.colorscheme 'tokyonight-moon'

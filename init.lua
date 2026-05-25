d8280af53e3c403209cc23456db2e030b1501c58
Jorge Gajon <gajon@gajon.org>
2026-05-24 22:54:46 -0600

    Added slim-template/vim-slim

--- init.lua
+++ init.lua
@@ -1040,6 +1040,8 @@ end
 -- kickstart.plugins.* examples
 -- ============================================================
 do
+  vim.pack.add { gh 'slim-template/vim-slim' }
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
